95340febc3b4be9d2463b9e669bb5784be1d6034
𝐌𝐞𝐥𝐢𝐬𝐬𝐚 𝐌𝐚𝐫𝐭𝐢𝐧𝐬 <martinsdossantosmelissa093@gmail.com>
2026-04-06 13:19:14 -0300

    Add files via upload

---
+++ Site Etec Zona Leste/script.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+let slides = document.getElementsByClassName("slide");
+
+function mostrarSlides(){
+
+for(let i = 0; i < slides.length; i++){
+slides[i].style.display = "none";
+}
+
+slideIndex++;
+
+if(slideIndex > slides.length){
+slideIndex = 1;
+}
+
+slides[slideIndex - 1].style.display = "block";
+
+setTimeout(mostrarSlides, 3000);
+
+}
+
+mostrarSlides();
